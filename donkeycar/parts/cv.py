e2bdf1339e2d3f405df4c4e022b4986ec9db8245
Will Roscoe <wroscoe@gmail.com>
2017-12-16 00:02:37 +0000

    big update with neural compute stic

--- donkeycar/parts/cv.py
+++ donkeycar/parts/cv.py
@@ -7,7 +7,63 @@ class ImgGreyscale():
         img_arr = cv2.cvtColor(img_arr, cv2.COLOR_RGB2GRAY)
         return img_arr
 
+class ImgDrawLine():
 
+    def __init__(self, start=(0,40),end=(160,40),color=(255,0,0), width=5):
+        self.start = start
+        self.end = end
+        self.color = color
+        self.width = width
+        
+        
+    def run(self, img_arr):
+        #img_arr = cv2.line(img_arr,(0,40),(160,40),(255,0,0),5)
+        img_arr = cv2.line(img_arr,self.start, self.end,self.color,self.width,cv2.LINE_AA)
+        return img_arr
+
+class ImgDrawArrow():
+
+    def __init__(self, start=(0,40),end=(160,40),color=(255,0,0), width=5):
+        self.start = start
+        self.end = end
+        self.color = color
+        self.width = width
+        
+        
+    def run(self, img_arr):
+        #img_arr = cv2.line(img_arr,(0,40),(160,40),(255,0,0),5)
+        img_arr = cv2.larrowedLine(img_arr,self.start, self.end,self.color,self.width,cv2.LINE_AA)
+        return img_arr
+
+class ImgPutInfo():
+
+    def __init__(self, text=('text here'), org=(0,20),color=(255,255,255), width=1):
+        self.org = org
+        self.text = text
+        self.color = color
+        self.width = width
+        
+        
+    def run(self, img_arr, throttle,angle):
+        img_arr = cv2.putText(img_arr,'SPD: '+str(throttle), (1,15), cv2.FONT_HERSHEY_SIMPLEX,0.5, self.color,self.width, cv2.LINE_AA)
+        img_arr = cv2.putText(img_arr,'ANG: '+str(angle), (1,35), cv2.FONT_HERSHEY_SIMPLEX,0.5, self.color,self.width, cv2.LINE_AA)
+
+        return img_arr
+
+class ImgPutText():
+
+    def __init__(self, text=('text here'), org=(0,20),color=(255,255,255), size=0.4, width=1):
+        self.org = org
+        self.text = text
+        self.color = color
+        self.width = width
+        self.size = size
+        
+        
+    def run(self, img_arr, classificaiton):
+        img_arr = cv2.putText(img_arr,classificaiton, (1,15), cv2.FONT_HERSHEY_SIMPLEX,self.size, self.color,self.width, cv2.LINE_AA)
+        
+        return img_arr
 
 class ImgCanny():
 
@@ -45,8 +101,8 @@ class ImgCrop:
         self.right = right
         
     def run(self, img_arr):
-        width, height, _ = img_arr.shape
-        img_arr = img_arr[self.top:height-self.bottom, 
+        height,width, _ = img_arr.shape
+        img_arr = img_arr[self.top:height-self.bottom,
                           self.left: width-self.right]
         return img_arr
         
